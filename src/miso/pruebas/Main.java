9b906a3026e84898b70132f422e3d81b6eb146b7
juan <juancamilogaviriaacosta@gmail.com>
2017-09-03 18:28:28 -0500

    Fin taller 4

--- src/miso/pruebas/Main.java
+++ src/miso/pruebas/Main.java
@@ -1,7 +1,6 @@
 package miso.pruebas;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import org.jsoup.Jsoup;
@@ -10,7 +9,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
- * @author juan
+ * @author Juan Camilo Gaviria Acosta - 201528434
  */
 public class Main {
 
@@ -19,20 +18,10 @@ public class Main {
      */
     public static void main(String[] args) {
         Main m = new Main();
-        //m.consultaTopPagasFinanzas();
-        m.consultaTopTodas();
+        m.consultaTopPagasFinanzas();
     }
 
     public void consultaTopPagasFinanzas() {
-        try {
-            Document doc = Jsoup.connect("https://play.google.com/store/apps/category/FINANCE/collection/topselling_paid").timeout(0).get();
-            System.out.println(doc);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void consultaTopTodas() {
         try {
             Set<String> hrefs = new HashSet<>();
             Document doc = Jsoup.connect("https://play.google.com/store/apps/category/FINANCE/collection/topselling_paid").timeout(0).get();
@@ -44,8 +33,36 @@ public class Main {
 
             for (String url : hrefs) {
                 Document paginaApp = Jsoup.connect(url).timeout(0).get();
+
+                //Descripcion
+                Elements claseTitulo = paginaApp.getElementsByClass("id-app-title");
+                System.out.println("\nTitulo: " + claseTitulo.get(0).text());
+
+                //Numero de raitings
+                Elements claseRaitings = paginaApp.getElementsByClass("rating-count");
+                System.out.println("Numero de raitings: " + claseRaitings.get(0).text());
+
+                //Score
+                Elements claseScore = paginaApp.getElementsByClass("score");
+                System.out.println("Score: " + claseScore.get(0).text());
+
+                //Descripcion
                 String descripcion = paginaApp.select("[itemprop='description']").text();
-                System.out.println(descripcion + "\n");
+                System.out.println("Descripcion: " + descripcion);
+
+                //Cambios recientes
+                Elements claseCambiosRecientes = paginaApp.getElementsByClass("recent-change");
+                System.out.println("Cambios recientes: " + claseCambiosRecientes.get(0).text());
+
+                //Ratings con 5 estrellas
+                Elements claseCincoEstrellas = paginaApp.getElementsByClass("rating-bar-container five");
+                System.out.println("Ratings con 5 estrellas: " + claseCincoEstrellas.get(0).text());
+
+                //Ratings con 4 estrellas
+                Elements claseCuatroEstrellas = paginaApp.getElementsByClass("rating-bar-container four");
+                System.out.println("Ratings con 4 estrellas: " + claseCuatroEstrellas.get(0).text());
+
+                break;
             }
         } catch (IOException e) {
             e.printStackTrace();
